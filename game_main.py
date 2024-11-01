527b4bb63fbb7d5ed77f772b9bbfc0b92c48f012
masonjones103 <masonjones103@gmail.com>
2024-10-31 17:53:00 -0700

    v0000.1
    Added some code from another file I made. Menu and stat screen are working.

--- game_main.py
+++ game_main.py
@@ -9,6 +9,47 @@
 #         ['[', '=', '=', '=', '=', '=', '=', ']'],
 #         ]
 #prints each individual character in a row using a for loop
+import os
+
+hp = 10
+dmg = 1
+weapon = 'Club'
+armor = 'Leather'
+
+def stats():
+    print('[====================]')
+    print(f'[ HP = {hp}          '.ljust(21, ' '), end='')
+    print(']')
+    print(f'[ DMG = {dmg}        '.ljust(21, ' '), end='')
+    print(']')
+    print(f'[ Weapon = {weapon}  '.ljust(21, ' '), end='')
+    print(']')
+    print(f'[ Armor = {armor}    '.ljust(21, ' '), end='')
+    print(']')
+    print('[====================]')
+    print('''
+''')
+
+def menu():
+    print('>----------<')
+    print('(P)lay')
+    print('(E)xit')
+    print('>----------<')
+    Play = False
+    Exit = False
+    while Play == False and Exit == False:
+        menu_choice = input('>').lower().strip()
+        if menu_choice == 'p':
+            Play = True
+        elif menu_choice == 'e':
+            Exit = True
+        else:
+            print('Please type (P) to play or (E) to exit.')
+    if Play == True:
+        print('Welcome to the Dungeon of Doom...')
+    elif Exit == True:
+        quit()
+
 def print_row(row):
     for char in row:
         print(char, end='')
@@ -39,14 +80,18 @@ row1 = ['[', '=', '=', '-', '=', '=', ']']
 row2 = ['[', ' ', ' ', ' ', ' ', ' ', ']']
 #used to store the changed character
 updated_row1 = ['[', '=', '=', '-', '=', '=', ']']
-
+updated_row2 = ['[', ' ', ' ', ' ', ' ', ' ', ']']
 # for char in room1:
 #     print(char, end='')
 # for char in room2:
 #     print(char)
 
-print_room(row1)
-change_char(updated_row1, 3, '@')
-print_room(updated_row1)
-change_char(updated_row1, 4, '@')
-print_room(updated_row1)
+# print_room(row1)
+# change_char(updated_row2, 2, '@')
+# print_room(updated_row2)
+# updated_row2 = row2
+# change_char(updated_row2, 3, '@')
+# print_room(updated_row2)
+menu()
+stats()
+end = input('')

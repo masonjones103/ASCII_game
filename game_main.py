7c2051190acce290790ce66d6f517d5354f7c5a0
masonjones103 <masonjones103@gmail.com>
2024-11-14 22:07:41 -0800

    v0.01
    Took player symbol '@' out of list to store in a separate list (position).
    Added player movement, where the player moves the symbol under them is stored in stored_char.
    Added a function to run the game.
    Current bug with room bounds, sometimes loops, other times returns error.

--- game_main.py
+++ game_main.py
@@ -1,27 +1,29 @@
-# Hello, this is the starting code for my WIP ASCII game.
+''' Hello, this is the starting code for my WIP ASCII game.
+
+#TO-DO:
+ - FIX: Occasional Out-Of-Bounds Error(?)
+ - Add bounds to the room
+#DOING:
+ - 
+
+#DONE:
+ -
+
+'''
 
 #import os
 
+#stores the row and index of the player character. [4, 3] is the starting position of the '@' character.
+position = [4, 3]
+stored_char = [4, 3, ' ']
+
+#the player's stat values
 hp = 10
 dmg = 1
 weapon = 'Club'
 armor = 'Leather'
 
-#prints stat menu, uses .ljust to keep boundaries even regardless of variable length
-def stats():
-    print('[====================]')
-    print(f'[ HP = {hp}          '.ljust(21, ' '), end='')
-    print(']')
-    print(f'[ DMG = {dmg}        '.ljust(21, ' '), end='')
-    print(']')
-    print(f'[ Weapon = {weapon}  '.ljust(21, ' '), end='')
-    print(']')
-    print(f'[ Armor = {armor}    '.ljust(21, ' '), end='')
-    print(']')
-    print('[====================]')
-    print('''
-''')
-
+#prints a menu where the player can select to play or exit the game
 def menu():
     print('>----------<')
     print('(P)lay')
@@ -42,90 +44,82 @@ def menu():
     elif exit_menu:
         quit()
 
-#accesses the nested list to print each character in the chosen list
-def print_room(room, row):
-    i = 0
-    for char in range(len(room[row])):
-        print(room[row][i], end='')
-        i += 1
-    print('')
+#prints stat menu, uses .ljust to keep boundaries even regardless of variable length
+def stats():
+    print('[====================]')
+    print(f'[ HP = {hp}          '.ljust(21, ' '), end='')
+    print(']')
+    print(f'[ DMG = {dmg}        '.ljust(21, ' '), end='')
+    print(']')
+    print(f'[ Weapon = {weapon}  '.ljust(21, ' '), end='')
+    print(']')
+    print(f'[ Armor = {armor}    '.ljust(21, ' '), end='')
+    print(']')
+    print('[====================]')
+    print('''
+''')
 
-#calls print_row to print multiple rows, need to figure out how to iterate through each row
-# def print_room(row):
-#     #row_num = 1
-#     for row_num in range(1):
-#         print_row(row)
-#         print('')
-#         #row_num += 1
+#takes the room number, then prints each character in a row, iterates to the next row, and continues until all rows are printed.
+def print_room(room):
+    for row in room:
+        print('')
+        for char in row:
+            print(char, end='')
+    print('''
+    ''')
 
-#updates the inputted row index to match the inputted character
-def change_char(row, index, char):
+#takes the room, row, and index numbers and changes the given spot to the char.
+def change_char(room, row, index, char):
     value = True
     while value:
-        row[index] = char
-        #print_row(row)
-        #print('')
+        room[row][index] = char
         value = False
 
-# def player_cords(room, row, index):
-#     if room1.find('@') != -1:
-#         player_position = room[row][index]
-#         return player_position
+def move_char():
+    move_input = input("Press 'W' to move forward, 'S' to move back, 'A' to move left, or 'D' to move right.\n>").upper().strip()
+    if move_input == 'W':
+        position[0] = position[0] - 1
+    elif move_input == 'S':
+        position[0] = position[0] + 1
+    elif move_input == 'A':
+        position[1] = position[1] -1
+    elif move_input == 'D':
+        position[1] = position[1] + 1
+    else:
+        move_char()
 
-#searches each row to see if the '@' character is found. If it is, it returns that index, else it returns -1
-def find_row(row):
-    search = True
-    while search:
-        try:
-            return room1[row].index('@')
-        except ValueError:
-            return -1
+def update_char(room):
+    row = position[0]
+    char = position[1]
+    stored_char[0] = position[0]
+    stored_char[1] = position[1]
+    stored_char[2] = room[row][char]
+    room[row][char] = '@'
 
-#iterates through each row, then uses find row to find the index of '@'
-def find_pos():
-    row = 0
-    pos = 0
-    search = True
-    while search:
-        if find_row(row) == -1:
-            row += 1
-            find_row(row)
-        else:
-            pos = find_row(row)
-            return pos
+def return_stored(room):
+    row = stored_char[0]
+    char = stored_char[1]
+    symbol = stored_char[2]
+    room[row][char] = symbol
 
-#def update_position():
-
-#def clear_row():
-
-#the stored rows in a list, I'm hoping to use these to refresh screen to erase previous prints
-row1 = ['[', '=', '=', '-', '=', '=', ']']
-row2 = ['[', ' ', ' ', ' ', ' ', ' ', ']']
-#used to store the changed character
-updated_row1 = ['[', '=', '=', '-', '=', '=', ']']
-updated_row2 = ['[', ' ', ' ', ' ', ' ', ' ', ']']
+def play_game(room):
+    menu()
+    stats()
+    update_char(room)
+    print_room(room)
+    playing = True
+    while playing:
+        move_char()
+        return_stored(room)
+        update_char(room)
+        print_room(room)
 
 room1 = [['[', '=', '=', '-', '=', '=', ']'],
-         ['[', ' ', ' ', ' ', '@', ' ', ']'],
+         ['[', ' ', ' ', 'g', ' ', '^', ']'],
+         ['[', '^', ' ', ' ', ' ', ' ', ']'],
+         ['[', ' ', ' ', ' ', ' ', '^', ']'],
+         ['[', '^', ' ', ' ', ' ', ' ', ']'],
+         ['[', '=', '=', '-', '=', '=', ']'],
          ]
-# for char in room1:
-#     print(char, end='')
-# for char in room2:
-#     print(char)
 
-# print_room(row1)
-# change_char(updated_row2, 2, '@')
-# print_room(updated_row2)
-# updated_row2 = row2
-# change_char(updated_row2, 3, '@')
-# print_room(updated_row2)
-# menu()
-# stats()
-# end = input('')
-print_room(room1, 0)
-print_room(room1, 1)
-#print(player_cords(room1, 1, 3))
-test_list = ['=', '@', '=']
-# print(test_list.index('@'))
-# print(room1[1].index('@'))
-print(find_pos())
+play_game(room1)

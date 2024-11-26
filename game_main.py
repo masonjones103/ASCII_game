030f9e4ed1d0c14d9d5adc2a86c0a271f94ef6a6
masonjones103 <masonjones103@gmail.com>
2024-11-25 18:09:57 -0800

    v0.3
    Changed how combat works and added a rudimentary system. Need to fix some bugs and break down the combat function.

--- game_main.py
+++ game_main.py
@@ -2,17 +2,20 @@
 
 #TO-DO:
  - FIX: Occasional Out-Of-Bounds Error(?)
- - Add treasure room
  - Add new equipment
+ - Make movement one button press
+ - Fix combat so that goblin does not change attacks when the player inputs the wrong value
 #DOING:
- -Adding combat and making the goblin in ASCII
-
+ -Adding combat
+ - Add treasure room
 #DONE:
  - Added bounds to the room
  - Added collision to enemy
+ - Made goblin in ASCII
 '''
 
 import os
+import random
 
 # stores the row and index of the player character. [4, 3] is the starting position of the '@' character.
 position = [4, 3]
@@ -41,7 +44,7 @@ bounds = [
 ]
 
 # a very scary goblin. For some reason line 47 reads normal if the terminal if I run it offset like that.
-goblin = r'''
+goblin1 = r'''
                             /\     /\
 	                   (  \___/  )
                             |       |
@@ -61,21 +64,48 @@ goblin = r'''
                             ||     ||
                            /  \   /  \
                            ^^^^   ^^^^
-                           
+'''
+
+goblin_meet = r'''
             You meet face to face with the ferocious goblin.
               Will you (A)ttack the goblin or (R)un away?
 '''
 
+goblin_dead = r'''
+
+
+
+
+
+
+                                  ____
+                              ___/   /
+                             /     x |
+                         ~~##| d   x \
+                      ~~~~~~~~~~~~~~~~~~~~
+
+Your swing connects with the goblin's neck and his head goes flying, decapitated.
+'''
+
 # the player's stat values
-hp = 10
-dmg = 1
-weapon = 'Sword'
-armor = 'Leather'
+player_hp = 10
+player_eg = 10
+# dmg = '1-3'
+# weapon = 'Sword'
+# armor = 'Leather'
+
+# the enemy's stat values
+enemy_hp = 10
+enemy_eg = 10
+
+# # weapon damage ranges
+# sword_dmg = (1, 3)
+# cleaver_dmg = (1, 2)
 
 # set to False if the goblin dies
 enemy_alive = True
 
-# stores lines that happened upon certain conditions
+# stores lines that happen upon certain conditions
 lines = ['']
 
 # prints a menu where the player can select to play or exit the game
@@ -105,14 +135,16 @@ You grasp your sword and move forward cautiously...''')
 # prints stat menu, uses .ljust to keep boundaries even regardless of variable length
 def stats():
     print('[====================]')
-    print(f'[ HP = {hp}          '.ljust(21, ' '), end='')
+    print(f'[ Health = {player_hp} '.ljust(21, ' '), end='')
     print(']')
-    print(f'[ DMG = {dmg}        '.ljust(21, ' '), end='')
+    print(f'[ Energy = {player_eg} '.ljust(21, ' '), end='')
     print(']')
-    print(f'[ Weapon = {weapon}  '.ljust(21, ' '), end='')
-    print(']')
-    print(f'[ Armor = {armor}    '.ljust(21, ' '), end='')
-    print(']')
+    # print(f'[ Damage = {dmg}     '.ljust(21, ' '), end='')
+    # print(']')
+    # print(f'[ Weapon = {weapon}  '.ljust(21, ' '), end='')
+    # print(']')
+    # print(f'[ Armor = {armor}    '.ljust(21, ' '), end='')
+    # print(']')
     print('[====================]')
     print('''
 ''')
@@ -157,7 +189,7 @@ def print_lines(lines):
         print('')
     lines[0] = ''
 
-# updates the position list to give coordinates that your character will move to when update_char() is called.
+# updates the position list to give coordinates that your character will move to when update_char() is called. Very messy right now, I should clean it up and make more functions.
 def move_char():
     new_position = [4, 3]   # where the player is trying to move, does not actually move there unless check_bound() returns False
     previous_position = [4, 3]  # where the player was standing before checking if a wall was in the way.
@@ -174,7 +206,7 @@ def move_char():
         elif new_position == [1, 3] and enemy_alive:    # if you walk into the enemy, text you won't move and text will appear.
             position[0] = previous_position[0]
             position[1] = previous_position[1]
-            combat()
+            enemy_encounter()
         else:     # if there is no wall in the way, your move goes through and position is updated.
             position[0] = new_position[0]
     elif move_input == 'S':
@@ -190,7 +222,7 @@ def move_char():
         elif new_position == [1, 3] and enemy_alive:
             position[0] = previous_position[0]
             position[1] = previous_position[1]
-            combat()
+            enemy_encounter()
         else:
             position[0] = position[0] + 1
     elif move_input == 'A':
@@ -203,7 +235,7 @@ def move_char():
         elif new_position == [1, 3] and enemy_alive:
             position[0] = previous_position[0]
             position[1] = previous_position[1]
-            combat()
+            enemy_encounter()
         else:
             position[1] = position[1] -1
     elif move_input == 'D':
@@ -216,7 +248,7 @@ def move_char():
         elif new_position == [1, 3] and enemy_alive:
             position[0] = previous_position[0]
             position[1] = previous_position[1]
-            combat()
+            enemy_encounter()
         else:
             position[1] = position[1] + 1
     else:
@@ -250,21 +282,148 @@ def clear_screen():
         os.system('clear')
 
 # if you encounter the goblin, this function clears the screen and prints the scary goblin
-def combat():
+def enemy_encounter():
     clear_screen()
-    print(goblin)
+    print(goblin1)
+    print(goblin_meet)
     choice1 = input('>').upper().strip()
     if choice1 == 'A':
-        print("You stab the poor goblin through the heart and exit through the door behind it.")
-        pause = input("Congrats! You've won the game!")
-        quit()
+        attack = input("You inch towards the goblin with your sword drawn and ready yourself for combat.")
+        while player_hp > 0 or enemy_hp > 0:
+            combat()
+        print(f'Player HP: {player_hp}')
+        print(f'Player EG: {player_eg}')
+        print(f'Enemy HP: {enemy_hp}')
+        print(f'Enemy EG: {enemy_eg}')
+        pause = input('')
     elif choice1 == 'R':
         print("You turn around to run, the goblin lunges onto your back and stabs you repeatedly until you die.")
         print("Game over...")
         pause = input('')
         quit()
     else:
-        combat()
+        enemy_encounter()
+
+# determines the enemy's stance based of a random dice roll from 1-3.
+def enemy_stance():
+    rand_stance = random.randint(1, 3)
+    if rand_stance == 1:
+        stance = 'lightattack'
+        return stance
+    elif rand_stance == 2:
+        stance = 'heavyattack'
+        return stance
+    elif rand_stance == 3:
+        stance = 'parry'
+        return stance
+
+# gets the player's combat choice input
+def player_stance():
+    combat_choice = input('''                   What would you like to prepare for?     
+                (L)ight attack, (H)eavy attack, or (P)arry?\n>''').upper().strip()
+    if combat_choice == 'L':
+        player_l_atk = True
+        return player_l_atk
+    elif combat_choice == 'H':
+        player_h_atk = True
+        return player_h_atk
+    elif combat_choice == 'P':
+        player_p = True
+        return player_p
+    else:
+        player_stance()
+
+# runs through combat. I need to clean it up and split it into more functions probably.
+def combat():
+    goblin = goblin1
+    enemy_l_atk = False
+    enemy_h_atk = False
+    enemy_p = False
+    player_l_atk = False
+    player_h_atk = False
+    player_p = False
+
+    clear_screen()
+    #stats()
+    print(goblin)
+    # gets the enemy's combat choice
+    stance = enemy_stance()
+    # prints the enemy's attack choice
+    if stance == 'lightattack':
+        print('''       
+                  The enemy is preparing a light attack.''')
+        enemy_l_atk = True
+    elif stance == 'heavyattack':
+        print('''       
+                The enemy is preparing for a heavy attack''')
+        enemy_h_atk = True
+    elif stance == 'parry':
+        print('''       
+                   The enemy is preparing to parry.''')
+        enemy_p = True
+    # gets the player's combat choice input
+    combat_choice = input('''                   What would you like to prepare for?     
+                (L)ight attack, (H)eavy attack, or (P)arry?\n>''').upper().strip()
+    if combat_choice == 'L':
+        player_l_atk = True
+    elif combat_choice == 'H':
+        player_h_atk = True
+    elif combat_choice == 'P':
+        player_p = True
+
+    global player_hp
+    global player_eg
+    global enemy_hp
+    global enemy_eg
+    # all the combinations of player and enemy attacks
+    if enemy_l_atk == True and player_l_atk == True:
+        pause = input('You both strike each other with a quick jab, drawing a small amount of blood.')
+        player_hp -= 1
+        enemy_hp -= 1
+    elif enemy_l_atk == True and player_h_atk == True:
+        pause = input('You prepare for a heavy strike, but the goblin darts in with a quick jab before you can react.')
+        player_hp -= 1
+        player_eg -= 3
+    elif enemy_l_atk == True and player_p == True:
+        pause = input('The goblin strikes at you with a quick jab, but you easily parry the light blow, striking back with a stab.')
+        player_eg -= 1
+        enemy_hp -= 1
+    elif enemy_h_atk == True and player_l_atk == True:
+        pause = input('The goblin lifts his cleaver high, preparing for a mighty strike, but you dart in a quick jab before before he can react.')
+        enemy_hp -= 1
+        enemy_eg -= 3
+    elif enemy_h_atk == True and player_h_atk == True:
+        pause = input('You both lift your weapons high and strike a vicious blow on each other, disregarding your personal safety.')
+        player_hp -= 3
+        player_eg -= 3
+        enemy_hp -= 3
+        enemy_eg -= 3
+    elif enemy_h_atk == True and player_p == True:
+        pause = input('The goblin strikes with a mighty blow, powering through your parry.')
+        player_hp -= 2
+        player_eg -= 1
+        enemy_eg -= 3
+    elif enemy_p == True and player_l_atk == True:
+        pause = input("You stab at the goblin with a light strike, but he easily parries the blow and lands a quick riposte.")
+        player_hp -= 1
+        enemy_eg -= 1
+    elif enemy_p == True and player_h_atk == True:
+        pause = input("Your lift your sword over your head and bring it down upon the goblin with a mighty blow, he tries to parry the blade, but it powers through his block.")
+        player_eg -= 3
+        enemy_hp -= 2
+        enemy_eg -= 1
+    elif enemy_p == True and player_p == True:
+        pause = input("You both steel yourself for an attack, nothing happens.")
+    if player_eg < 0:
+        player_hp += player_eg
+    if enemy_eg < 0:
+        enemy_hp += enemy_eg
+    if player_hp <= 0:
+        end = input('You die.')
+        quit()
+    if enemy_hp <= 0:
+        end = input('The enemy dies.')
+        quit()
 
 # the standard order of functions for moving around the map
 def order_of_play(room):
